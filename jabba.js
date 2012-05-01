9032a4cd4d6b8b1430dab95dafd49cd9c1f3ad64
Jean-Marc Skopek <jskopek@gmail.com>
2012-05-01 16:15:52 -0400

    Initial simple implementation

--- jabba.js
+++ jabba.js
@@ -1,1 +1,96 @@
+(function($) {
+    var generate_tab_id = function(tab_name) {
+        //returns a lowercased, truncated, _ed name
+        tab_name = tab_name.trim().toLowerCase();
+        tab_name = tab_name.replace(/\s+/g,"_").replace(/[\W\s]/g,"");
+        return tab_name;
+    }
+    var render_tab_title = function(el, tab_id, tab_name) {
+        //get or create tab title container
+        var tab_container_el = $(el).find(".nav-tabs");
+        if( !tab_container_el.length ) {
+            tab_container_el = $("<div class='nav nav-tabs'></div>");
+            $(el).append(tab_container_el);
+        }
 
+        //get or create tab title
+        var tab_el = $(el).find(".nav-tabs li").filter(function() {
+            return $(this).attr("href") == "#" + tab_id;
+        });
+        if( !tab_el.length ) {
+            tab_el = $("<li><a href='#" + tab_id + "'>" + tab_name + "</a></li>");
+            tab_container_el.append(tab_el);
+        }
+    }
+    var render_tab_body = function(el, tab_id, tab_content_el) {
+        //get or create tab body container
+        var tab_container_el = $(el).find(".tab-content");
+        if( !tab_container_el.length ) {
+            tab_container_el = $("<div class='tab-content'></div>");
+            $(el).append(tab_container_el);
+        }
+
+        //get or create tab body
+        var tab_el = $(el).find(".tab-content").find("#" + tab_id);
+        if( !tab_el.length ) {
+            tab_el = $("<div class='tab-pane' id='" + tab_id + "'></div>");
+            tab_container_el.append(tab_el);
+        }
+
+        if( tab_content_el ) {
+            tab_el.html( tab_content_el );
+        }
+    }
+    var methods = {
+        "init": function(tabs) {
+            for( var index in tabs ) {
+                var data = tabs[index];
+                //convert object into a simple JSON obj if it is not already
+                if( !(data instanceof Object) ) {
+                    data = {"title": data};
+                }
+                
+                //ensure we have a title
+                if( !data["title"] ) {
+                    throw("Could not find title in object: " + JSON.stringify(data) );
+                }
+                console.log("rendering", data);
+
+                //get data
+                var title = data["title"];
+                var id = data["id"] || generate_tab_id(title);
+                var el = data["el"] || undefined;
+
+                render_tab_title(this, id, title);
+                render_tab_body(this, id, el);
+            }
+
+            //select the first id
+            var first_id = $(this).find(".tab-content .tab-pane:first").attr("id");
+            methods.select.call(this, first_id)
+
+            console.log("jabba initialized on ", this, tabs);
+        },
+        "select": function(tab_id) {
+            //add active class to tab title
+            $(this).find(".nav-tabs li").removeClass("active");
+            $(this).find(".nav-tabs li a").filter(function() {
+                return $(this).attr("href") == "#" + tab_id;
+            }).parent().addClass("active");
+
+            //add active class to tab content
+            $(this).find(".tab-content .tab-pane").removeClass("active");
+            $(this).find(".tab-content .tab-pane").filter("#" + tab_id).addClass("active");
+        }
+    }
+
+    $.fn.jabba = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === "object" || ! method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error("Method " + method + " does not exist on jQuery.jabba");
+        }
+    }
+})(jQuery);
--- tests/testJabba.js
+++ tests/testJabba.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){
 
     test("initialize single simple jabba tab", function() {
-        var el = $("<div/>");
+        var el = $("<div id='tst'></div>");
         $("body").append(el);
 
         el.jabba(["Hello World"]);
         
-        equal( el.find(".nav-tabs li").length(), 2);
-        equal( el.find(".tab-content li").length(), 2);
+        equal( el.find(".nav-tabs li").length, 1);
+        equal( el.find(".tab-content .tab-pane").length, 1);
         equal( el.find(".nav-tabs li").text(), "Hello World");
 
+    });
+
+    test("initialize multi simple jabba tab", function() {
+        var el = $("<div id='tst'></div>");
+        $("body").append(el);
+
+        el.jabba(["Hello World", "Testing", "This"]);
+        
+        equal( el.find(".nav-tabs li").length, 3);
+        equal( el.find(".tab-content .tab-pane").length, 3);
+        equal( el.find(".nav-tabs li:last").text(), "This");
+
+    });
+
+    test("test auto jabba ids", function() {
+        var el = $("<div/>");
+        el.jabba(["Hello World"]);
+        equal( el.find(".tab-content .tab-pane").attr("id"), "hello_world");
+
+        var el = $("<div/>");
+        el.jabba([" multiple SPACES AND things    Hello World   "]);
+        equal( el.find(".tab-content .tab-pane").attr("id"), "multiple_spaces_and_things_hello_world");
+
+        var el = $("<div/>");
+        el.jabba(["12 numbers$*)#"]);
+        equal( el.find(".tab-content .tab-pane").attr("id"), "12_numbers");
     });
 });
