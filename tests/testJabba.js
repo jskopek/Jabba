5f9061a53a883a7d9c75dedeb4fe84cc396edd7a
Jean-Marc Skopek <jskopek@gmail.com>
2012-05-01 16:18:56 -0400

    Added complex tests

--- tests/testJabba.js
+++ tests/testJabba.js
@@ -24,6 +24,29 @@ $(document).ready(function(){
 
     });
 
+    test("initialize complex tabs", function() {
+        var el = $("<div id='tst'></div>");
+        $("body").append(el);
+
+        el.jabba([
+            {
+                "title": "Home",
+                "id": "custom_home_id",
+                "el": $("#home_el") //sets the content of the Home tab to #home_el on creation
+            },
+            {
+                "title": "Profile",
+                "id": "custom_profile_id"
+            },
+            "Messages"
+        ]);
+        
+        equal( el.find(".nav-tabs li").length, 3);
+        equal( el.find(".tab-content .tab-pane").length, 3);
+        equal( el.find(".nav-tabs li:first").text(), "Home");
+        equal( el.find(".nav-tabs li:last").text(), "Messages");
+    });
+
     test("test auto jabba ids", function() {
         var el = $("<div/>");
         el.jabba(["Hello World"]);
@@ -37,4 +60,11 @@ $(document).ready(function(){
         el.jabba(["12 numbers$*)#"]);
         equal( el.find(".tab-content .tab-pane").attr("id"), "12_numbers");
     });
+    test("test manual jabba ids", function() {
+        var el = $("<div/>");
+        el.jabba([{"title": "Hello World", "id": "test_id"}, "Test Two"]);
+        equal( el.find(".tab-content .tab-pane:first").attr("id"), "test_id");
+        equal( el.find(".tab-content .tab-pane:last").attr("id"), "test_two");
+    });
+
 });
